de20747e4169bf62bde4d71c496ef968ebc4dbb0
Andriy <andriimatrix@gmail.com>
2023-11-06 16:32:19 +0200

    Home work 16 actions and assertions

---
+++ tests/new_subscriber_form_test(negative).spec.js
@@ -0,0 +1,148 @@
+
+import { test, expect } from '@playwright/test';
+
+const redBorderColor = 'rgb(220, 53, 69)';
+
+test.describe("Sign-up form validation", ()=> {
+    test.beforeEach(async ({page}) => {
+        await page.goto('/');
+        //click on sign up
+        await page.locator('.hero-descriptor_btn.btn.btn-primary').click();
+    });
+
+    test("T-01 verify the name validation: empty and red border", async ({page})=> {
+        const userNameInput = page.locator('#signupName');
+        await userNameInput.click();
+        //focus out from the field
+        await page.locator('.modal-footer').click();
+
+        const nameWarningsPanel = page.locator('div.invalid-feedback');
+        await nameWarningsPanel.isVisible();
+
+        const firstErrorText = await page.locator('div.invalid-feedback p')
+            .nth(0).innerText();
+
+        const borderColor = await userNameInput.evaluate((el) => {
+            const style = window.getComputedStyle(el);
+            return style.getPropertyValue("border-color");
+        });
+
+        expect(firstErrorText).toStrictEqual('Name required');
+        expect(borderColor).toBe(redBorderColor);
+    });
+
+    test("T-02 verify the name validation: wrong data and length input", async ({page})=> {
+        const userNameInput = page.locator('#signupName');
+        await userNameInput.click();
+        await userNameInput.fill('1');
+        //focus out from the field
+        await page.locator('.modal-title').click();
+
+        const nameWarningsPanel = page.locator('div.invalid-feedback');
+        await nameWarningsPanel.isVisible();
+
+        const firstErrorText = await page.locator('div.invalid-feedback p')
+            .nth(0).innerText();
+        const secondErrorText = await page.locator('div.invalid-feedback p')
+            .nth(1).innerText();
+
+
+        expect(firstErrorText).toStrictEqual('Name is invalid');
+        expect(secondErrorText).toStrictEqual('Name has to be from 2 to 20 characters long');
+
+    });
+
+    test("T-03 verify the last name validation: wrong length input",
+        async ({page})=> {
+        const userLastNameInput = page.locator('#signupLastName');
+        await userLastNameInput.click();
+        await userLastNameInput.fill('B');
+        //focus out from the field
+        await page.locator('.modal-footer').click();
+
+        const lastNameWarningsPanel = page.locator('div.invalid-feedback');
+        await lastNameWarningsPanel.isVisible();
+
+        const firstErrorText = await page.locator('.invalid-feedback').innerText();
+
+        expect(firstErrorText).toStrictEqual('Last name has to be from 2 to 20 characters long');
+    });
+
+    test("T-04 verify the email validation: wrong input data", async ({page})=> {
+        const userEmailInput = page.locator('#signupEmail');
+        await userEmailInput.click();
+        await userEmailInput.fill('aqa-andriy.@mail.com');
+        //focus out from the field
+        await page.locator('.modal-footer').click();
+
+        const emailWarningsPanel = page.locator('div.invalid-feedback');
+        await emailWarningsPanel.isVisible();
+
+        const errorText = await page.locator('div.invalid-feedback').innerText();
+
+        expect(errorText).toStrictEqual('Email is incorrect');
+    });
+
+    test("T-05 verify the password validation: wrong input data", async ({page})=> {
+        const userPasswordInput = page.locator('#signupPassword');
+        await userPasswordInput.click();
+        await userPasswordInput.fill('12345678');
+        //focus out from the field
+        await page.locator('.modal-footer').click();
+
+        const emailWarningsPanel = page.locator('div.invalid-feedback');
+        await emailWarningsPanel.isVisible();
+
+        const errorText = await page.locator('.invalid-feedback').innerText();
+
+        expect(errorText).toStrictEqual('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
+
+    });
+
+    test("T-06 verify the re-enter password validation: wrong input data", async ({page})=> {
+        const userPasswordInput = page.locator('#signupPassword');
+        await userPasswordInput.click();
+        await userPasswordInput.fill('Ab12345678');
+
+        const userRepeatPasswordInput = page.locator('#signupRepeatPassword');
+        await userRepeatPasswordInput.click();
+        await userRepeatPasswordInput.fill('Ac12345678');
+        //focus out from the field
+        await page.locator('.modal-footer').click();
+
+        const emailWarningsPanel = page.locator('div.invalid-feedback');
+        await emailWarningsPanel.isVisible();
+
+        const errorText = await page.locator('.invalid-feedback').innerText();
+
+        expect(errorText).toStrictEqual('Passwords do not match');
+
+    });
+
+    test.afterEach(async ({page})=>{
+        //close the form
+        await page.locator('button.close').click();
+    });
+})
+
+    // await fieldName.fill(' ')
+    // await fieldName.fill('**')
+    // await fieldName.fill('123')
+    // await fieldName.fill('A12')
+    // await fieldName.fill('Aqa*')
+    // await fieldName.fill('true')
+    // await fieldName.fill('false')
+    // await fieldName.fill('Undefined')
+    // await fieldName.fill('Nun')
+    // await fieldName.fill('//')
+    // await fieldName.fill('AqaS_S')
+    // await fieldName.fill('Aqa*B')
+    // await fieldName.fill('A')
+    // await fieldName.fill('asdfghjklqwertyuiopmq')
+    // await fieldName.fill('.')
+    // await fieldName.fill('00')
+    // await fieldName.fill('aqa.jpeg')
+    // await fieldName.fill('Aqa.exe')
+    // await fieldName.fill('-Aqa')
+    // await fieldName.fill(' https://www.google.com.ua')
+    //
